a3ab265cc884592862983fb1ca32c5f5ce129597
Dominika <dominika.sporzynska@gmail.com>
2017-04-07 17:20:37 +0200

    Struktura projektu

---
+++ src/main/java/com/dominika/DemoEserwisikApplication.java
@@ -0,0 +1,12 @@
+package com.dominika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEserwisikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEserwisikApplication.class, args);
+	}
+}
